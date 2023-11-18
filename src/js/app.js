a3982531367328aa4d6274afa4d578c0a4aaee61
Jose A Muñoz Cardozo <119539448+joshemcr2@users.noreply.github.com>
2023-11-18 02:12:50 +0000

    First commit

--- src/js/app.js
+++ src/js/app.js
@@ -5,10 +5,10 @@ import "../style/index.css";
  *  This function is called every time the user changes types or changes any input
  * 
     {
-        includeCover: true, // if includeCover is true the algorithm should show the cover image
-        background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da", // this is the image's url that will be used as a background for the profile cover
+        includeCover: true, // if includeCover is true the algorithm should
+        background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da", // this is the url of the image that will used as background for the profile cover
         avatarURL: "https://randomuser.me/api/portraits/women/42.jpg", // this is the url for the profile avatar
-        socialMediaPosition: "right", // social media bar position (left or right)
+        socialMediaPosition: "left", // social media bar position (left or right)
         
         twitter: null, // social media usernames
         github: null,
@@ -16,14 +16,14 @@ import "../style/index.css";
         instagram: null,
 
         name: null,
-        lastName: null,
+        lastname: null,
         role: null,
         country: null,
         city: null
     }
  */
 function render(variables = {}) {
-  console.log("These are the current variables: ", variables); // print on the console
+  console.log("These are the current variables: ", variables); //print on the console
   // here we ask the logical questions to make decisions on how to build the html
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
@@ -33,14 +33,26 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${variables.name ? variables.name : " "} ${
+    variables.lastName ? variables.lastName : " "
+  }</h1>
+          <h2>${variables.role ? variables.role : " "} </h2>
+          <h3>${variables.city ? variables.city : " "} ${
+    variables.country ? variables.country : " "
+  }</h3>
+          <ul class="position ${variables.socialMediaPosition}">
+            <li><a href="https://twitter.com/${
+              variables.twitter
+            }" target="_blank"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${
+              variables.github
+            }"target="_blank"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/in/${
+              variables.linkedin
+            }"target="_blank"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${
+              variables.instagram
+            }"target="_blank"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
